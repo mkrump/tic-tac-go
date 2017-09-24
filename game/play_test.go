f1a681a0eef895d556d7127f8ec4f3aa05f3b95c
sc2nomore <matthewkrump@gmail.com>
2017-09-24 16:58:20 -0500

    Add CI and update README (#1)
    * Add CI
    
    * Update README

---
+++ game/play_test.go
@@ -0,0 +1,14 @@
+package game
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestPlay(t *testing.T) {
+	expected := "\n " +
+		"X | O | X \n" +
+		"   | O |  \n" +
+		"   | O |  \n"
+	assert.Equal(t, expected, Play(), "")
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/sc2nomore/tic-tac-go/game"
+)
+
+func main() {
+	fmt.Println(game.Play())
+}
