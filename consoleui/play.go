73774393ebad857dfebf5e566204d9cef53cca09
sc2nomore <32245561+sc2nomore@users.noreply.github.com>
2017-09-25 13:42:33 -0500

    Render board (#3)
    * Add Board rendering for console game

--- consoleui/play.go
+++
@@ -1,9 +0,0 @@
-package consoleui
-
-// Play starts new tic-tac-toe consoleui.
-func Play() string {
-	return "\n " +
-		"X | O | X \n" +
-		"   | O |  \n" +
-		"   | O |  \n"
-}
---
+++ ui/consoleui_test.go
@@ -0,0 +1,48 @@
+package ui
+
+import (
+	"github.com/sc2nomore/tic-tac-go/core"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestRenderingEmpty3x3Board(t *testing.T) {
+	expected := "\n" +
+		"     |     |     \n" +
+		"- - - - - - - - -\n" +
+		"     |     |     \n" +
+		"- - - - - - - - -\n" +
+		"     |     |     \n"
+	board := core.MakeBoard(3)
+	assert.Equal(t, expected, RenderBoard(board), "")
+}
+
+func TestRenderingNonEmpty3x3Board(t *testing.T) {
+	expected := "\n" +
+		"     |  X  |     \n" +
+		"- - - - - - - - -\n" +
+		"     |  O  |     \n" +
+		"- - - - - - - - -\n" +
+		"     |     |  X  \n"
+	board := core.MakeBoard(3)
+	board.MakeMove(1, 1)
+	board.MakeMove(4, -1)
+	board.MakeMove(8, 1)
+	assert.Equal(t, expected, RenderBoard(board), "")
+}
+
+func TestRenderingNonEmpty4x4Board(t *testing.T) {
+	expected := "\n" +
+		"     |  X  |     |     \n" +
+		"- - - - - - - - - - - -\n" +
+		"     |  O  |     |     \n" +
+		"- - - - - - - - - - - -\n" +
+		"     |     |  X  |     \n" +
+		"- - - - - - - - - - - -\n" +
+		"     |     |     |     \n"
+	board := core.MakeBoard(4)
+	board.MakeMove(1, 1)
+	board.MakeMove(5, -1)
+	board.MakeMove(10, 1)
+	assert.Equal(t, expected, RenderBoard(board), "")
+}
